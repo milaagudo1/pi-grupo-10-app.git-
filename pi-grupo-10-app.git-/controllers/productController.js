a0a10a0475b6cc1b598d1774c659049e9eb333a8
LuzmilaC <lcenturion@udesa.edu.ar>
2025-09-22 21:35:51 -0300

    modifique controller

--- pi-grupo-10-app.git-/controllers/productController.js
+++ pi-grupo-10-app.git-/controllers/productController.js
@@ -2,56 +2,41 @@
 const express = require('express');
 const localData = require('../localData/localData');
 
-const controladorProductoDetalle = {
+const controladorProducto = {
     show: function(req, res) {
-                const logueado = true;
-        let producto = localData.productos[0]; // producto estático
-        let usuario = localData.usuario;
-        return res.render("productDetail", { producto: producto, logueado, localData, usuario});
-    }
-};
-
-
-const controladorProductoBusqueda = {
-    results: function(req, res) {
-        const logueado = false;
-        return res.render("searchResults", { productos: localData.productos, logueado });
-    }
-};
-
-
-const controladorAddProduct = {
-    add: function(req, res) {
         const logueado = true;
-        return res.render("addProduct", { title: "express", logueado});
+        let producto = localData.productos[0];
+        let usuario = localData.usuario;
+        return res.render("productDetail", { producto:producto, logueado, localData, usuario });
+    },      
+
+    results: function(req, res) {
+        const logueado = false; 
+        return res.render("searchResults", { productos: localData.productos, logueado });
+    },
+
+    add: function(req, res) {
+        const logueado = true; 
+        return res.render("addProduct", { title: "express", logueado });
+    },
+
+    edit: function(req, res) {
+        const logueado = true; 
+        let id = req.params.id;
+        let indice = id - 1;
+        let product = localData.productos[indice];
+        let usuario = localData.usuario;
+
+
+        if (!product) {
+            return res.send("No existe un producto con ese ID");
+        }
+
+        return res.render("product-edit", { product, logueado, usuario });
     }
 };
 
+module.exports = controladorProducto;
 
-const controladorProductoEdicion = {
-  edit: function(req, res) {
-    const logueado = true;
-    let id = req.params.id;
-    let indice = id - 1;
-    let product = localData.productos[indice];
-
-
-
-
-    if (!product) {
-      return res.send("No existe un producto con ese ID");
-    }
- 
-        // Renderizamos la vista con el producto
-    return res.render("product-edit", { product, logueado,});
-  }
-};
-
-module.exports = {
-    show: controladorProductoDetalle.show,
-    results: controladorProductoBusqueda.results,
-    add: controladorAddProduct.add,
-    edit: controladorProductoEdicion.edit
-};
 
 
